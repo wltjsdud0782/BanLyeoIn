674a9a73a65fb7fb223195237f3ef249fc610879
wltjsdud0782 <wltjsdud0782@gmail.com>
2024-06-04 13:48:55 +0900

    최초

---
+++ src/main/java/com/dangsya/BanLyeoIn/BanLyeoInApplication.java
@@ -0,0 +1,13 @@
+package com.dangsya.BanLyeoIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanLyeoInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanLyeoInApplication.class, args);
+	}
+
+}

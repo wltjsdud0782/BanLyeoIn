a99a53782bd9e41e4c5d89bef6a3f02ae9a5fe2a
ejhong90 <hog215@naver.com>
2024-06-12 22:29:56 +0900

    work

--- src/main/java/com/dangsya/BanLyeoIn/member/controller/MemberController.java
+++ src/main/java/com/dangsya/BanLyeoIn/member/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.dangsya.BanLyeoIn.member.controller;
 import com.dangsya.BanLyeoIn.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +13,15 @@ public class MemberController {
 
     @Resource(name = "memberService")
     private MemberService memberService;
+
+    @GetMapping("/goJoin")
+    public String goJoin(){
+        return "content/member/join";
+    }
+
+    @PostMapping("/join")
+    public String join(){
+        return "redirect://soulmate";
+    }
 
 }
